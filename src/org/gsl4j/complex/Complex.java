ce4267e14230f5a17dbb8f72cd4b0b83073b6fa2
javaNoviceProgrammer <meisam@cpe-104-162-226-195.nyc.res.rr.com>
2020-06-01 09:00:15 -0400

    some classes added.

---
+++ src/org/gsl4j/complex/Complex.java
@@ -0,0 +1,11 @@
+package org.gsl4j.complex;
+
+import org.gsl4j.util.NativeLibraryLoader;
+
+public class Complex {
+
+	static {
+		NativeLibraryLoader.loadLibraries();
+	}
+
+}
---
+++ src/org/gsl4j/complex/ComplexMath.java
@@ -0,0 +1,11 @@
+package org.gsl4j.complex;
+
+import org.gsl4j.util.NativeLibraryLoader;
+
+public class ComplexMath {
+
+	static {
+		NativeLibraryLoader.loadLibraries();
+	}
+
+}
