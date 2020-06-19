a857e357db3b1f6374f46b1676ddc5879c720b4b
javaNoviceProgrammer <meisam@cpe-104-162-226-195.nyc.res.rr.com>
2020-06-19 19:17:08 -0400

    All of the special functions added.

---
+++ src/org/gsl4j/statistics/Stats.java
@@ -0,0 +1,17 @@
+package org.gsl4j.statistics;
+
+import org.gsl4j.util.NativeLibraryLoader;
+
+public class Stats {
+
+	static {
+		NativeLibraryLoader.loadLibraries();
+	}
+
+	private Stats() {
+
+	}
+
+
+
+}
