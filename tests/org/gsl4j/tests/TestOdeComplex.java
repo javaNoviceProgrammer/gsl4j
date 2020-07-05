bb84b31c33a32b902f36e434ad5bd88c351a300c
javaNoviceProgrammer <meisam@cpe-104-162-226-195.nyc.res.rr.com>
2020-07-05 15:12:55 -0400

    ode solver complex updated.

--- tests/org/gsl4j/tests/TestOdeComplex.java
+++ tests/org/gsl4j/tests/TestOdeComplex.java
@@ -16,13 +16,28 @@ public class TestOdeComplex {
 		double x0 = 0.0 ;
 		Complex y0 = 1.0+j ;
 		OdeSolverComplex odeSolver = new OdeSolverComplex(func, x0, y0) ;
-		double[] x = MathUtils.linspace(0.0, 20.0, 1000) ;
+		double[] x = MathUtils.linspace(0.0, 20.0, 10) ;
 		double[][] z = odeSolver.rkf45(x) ;
 		System.out.println(Arrays.toString(z[0]));
+		System.out.println(Arrays.toString(z[1]));
+	}
+
+	public static void test2() {
+		// y' = -y^2, y(0) = 1+j
+		DerivFunctionComplex func = (x,y) -> -y*y ;
+		DerivFunctionComplex dfdx = (x,y) -> 0.0+0.0*j ;
+		DerivFunctionComplex dfdy = (x,y) -> -2.0*y ;
+		double x0 = 0.0 ;
+		Complex y0 = 1.0+j ;
+		OdeSolverComplex odeSolver = new OdeSolverComplex(func, dfdx, dfdy, x0, y0) ;
+		double[] x = MathUtils.linspace(0.0, 20.0, 10) ;
+		Complex[] z = odeSolver.rk4impComplex(x) ;
+		System.out.println(Arrays.toString(z));
 	}
 
 	public static void main(String[] args) {
 		test1() ;
+		test2() ;
 	}
 
 }
