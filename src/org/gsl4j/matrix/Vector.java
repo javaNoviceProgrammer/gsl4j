069c55c0402cf7bfbe456a375671fa54578ed8c5
javaNoviceProgrammer <meisam@cpe-104-162-226-195.nyc.res.rr.com>
2020-07-06 07:14:51 -0400

    vector added.

--- src/org/gsl4j/matrix/Vector.java
+++ src/org/gsl4j/matrix/Vector.java
@@ -1,5 +1,19 @@
 package org.gsl4j.matrix;
 
+import org.gsl4j.util.NativeLibraryLoader;
+
 public class Vector {
+
+	static {
+		NativeLibraryLoader.loadLibraries();
+	}
+
+	double[] x ;
+	int dim ;
+
+	public Vector(double[] x) {
+		this.x = x ;
+		this.dim = x.length ;
+	}
 
 }
