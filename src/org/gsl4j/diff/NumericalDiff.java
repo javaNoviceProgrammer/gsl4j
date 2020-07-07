93b6d57152714a57e2f564e53a2b40b5f4b3b8fc
javaNoviceProgrammer <meisam@cpe-104-162-226-195.nyc.res.rr.com>
2020-07-06 20:26:59 -0400

    numerical diff updated.

--- src/org/gsl4j/diff/NumericalDiff.java
+++ src/org/gsl4j/diff/NumericalDiff.java
@@ -32,6 +32,8 @@ public class NumericalDiff {
 	 */
 	public static native double central(MathFunction func, double x, double h) ;
 
+	public static native double[] central(MathFunction func, double[] x, double h) ;
+
 	/**
 	 * This function computes the numerical derivative of the function f at the point x using an adaptive forward difference algorithm with a step-size of h. The function is evaluated only at points greater than x, and never at x itself. The derivative is returned in result and an estimate of its absolute error is returned in abserr. This function should be used if f(x) has a discontinuity at x, or is undefined for values less than x.
 	 * The initial value of h is used to estimate an optimal step-size, based on the scaling of the truncation error and round-off error in the derivative calculation. The derivative at x is computed using an “open” 4-point rule for equally spaced abscissae at x+h/4, x + h/2, x + 3h/4, x+h, with an error estimate taken from the difference between the 4-point rule and the corresponding 2-point rule x+h/2, x+h.
@@ -42,6 +44,8 @@ public class NumericalDiff {
 	 */
 	public static native double forward(MathFunction func, double x, double h) ;
 
+	public static native double[] forward(MathFunction func, double[] x, double h) ;
+
 	/**
 	 * This function computes the numerical derivative of the function f at the point x using an adaptive backward difference algorithm with a step-size of h. The function is evaluated only at points less than x, and never at x itself. The derivative is returned in result and an estimate of its absolute error is returned in abserr. This function should be used if f(x) has a discontinuity at x, or is undefined for values greater than x.
 	 * This function is equivalent to calling gsl_deriv_forward() with a negative step-size.
@@ -51,6 +55,8 @@ public class NumericalDiff {
 	 * @return
 	 */
 	public static native double backward(MathFunction func, double x, double h) ;
+
+	public static native double[] backward(MathFunction func, double[] x, double h) ;
 
 	/**
 	 * This function computes the numerical derivative of the function f at the point x using an adaptive central difference algorithm with a step-size of h. The derivative is returned in result and an estimate of its absolute error is returned in abserr.
--- tests/org/gsl4j/tests/TestNumericalDiff.java
+++ tests/org/gsl4j/tests/TestNumericalDiff.java
@@ -55,15 +55,33 @@ class TestNumericalDiff {
 		System.out.println(Arrays.toString(diff));
 	}
 
+	public static void test9() {
+		MathFunction func = t -> Math.sin(t) ;
+		double[] x = {1.1, 2.2, 3.3} ;
+		for(double v : x)
+			System.out.println(NumericalDiff.central(func, v, 0.1));
+		double[] diff = NumericalDiff.central(func, x, 0.1) ;
+		System.out.println(Arrays.toString(diff));
+		for(double v : x)
+			System.out.println(NumericalDiff.forward(func, v, 0.1));
+		diff = NumericalDiff.forward(func, x, 0.1) ;
+		System.out.println(Arrays.toString(diff));
+		for(double v : x)
+			System.out.println(NumericalDiff.backward(func, v, 0.1));
+		diff = NumericalDiff.backward(func, x, 0.1) ;
+		System.out.println(Arrays.toString(diff));
+	}
+
 	public static void main(String[] args) {
-		test1() ;
-		test2() ;
-		test3() ;
-		test4() ;
-		test5() ;
-		test6() ;
-		test7() ;
-		test8() ;
+//		test1() ;
+//		test2() ;
+//		test3() ;
+//		test4() ;
+//		test5() ;
+//		test6() ;
+//		test7() ;
+//		test8() ;
+		test9() ;
 	}
 
 }
