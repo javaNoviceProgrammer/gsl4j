33318be23524a78d67d72f4f84ef30c4fa369fe2
javaNoviceProgrammer <meisam@cpe-104-162-226-195.nyc.res.rr.com>
2020-07-10 08:04:06 -0400

    eigen system updated.

--- src/org/gsl4j/eigen/EigenSystem.java
+++ src/org/gsl4j/eigen/EigenSystem.java
@@ -13,6 +13,10 @@ public class EigenSystem {
 	public static final int EIGEN_SORT_ABS_ASC = 2 ;
 	public static final int EIGEN_SORT_ABS_DESC = 3 ;
 
+	private EigenSystem() {
+
+	}
+
 	// Real Symmetric Matrices
 	public static native double[] eigenSymm(double[][] matrix) ;
 	public static native double[] eigenSymmv(double[][] matrix, double[][] eigenVectors) ;
@@ -30,12 +34,12 @@ public class EigenSystem {
 	public static native double[] eigenGenSymmv(double[][] matrixA, double[][] matrixB, double[][] eigenVectors) ;
 
 	// Complex Generalized Hermitian-Definite Eigensystems
-
-
+	public static native double[] eigenGenHerm(double[][] matrixAreal, double[][] matrixAimag, double[][] matrixBreal, double[][] matrixBimag) ;
+	public static native double[] eigenGenHermv(double[][] matrixAreal, double[][] matrixAimag, double[][] matrixBreal, double[][] matrixBimag, double[][] eigenVecReal, double[][] eigenVecImag) ;
 
 	// Real Generalized Nonsymmetric Eigensystems
-
-
+	public static native void eigenGen(int computeS, int computeT, int balance, double[][] matrixA, double[][] matrixB, double[] vecAlphaReal, double[] vecAlphaImag, double[] beta) ;
+	public static native void eigenGenv(double[][] matrixA, double[][] matrixB, double[] vecAlphaReal, double[] vecAlphaImag, double[] beta, double[][] eigenVecReal, double[][] eigenVecImag) ;
 
 	// Sorting Eigenvalues and Eigenvectors
 
